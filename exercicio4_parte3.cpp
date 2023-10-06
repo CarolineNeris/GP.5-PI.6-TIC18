95846fcd90c8276d7dd0b336592d89f3fba98cf8
Matheus Beckerath <matheus.beckerath@hotmail.com>
2023-10-06 15:03:09 -0300

    Adicionando atv4 parte 3

---
+++ exercicio4_parte3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double x, y;
+
+    cout << "Digite o valor de x: ";
+    cin >> x;
+    cout << "Digite o valor de y: ";
+    cin >> y;
+
+    string quadrante = (x > 0 && y > 0) ? "primeiro quadrante" :
+                       (x < 0 && y > 0) ? "segundo quadrante" :
+                       (x < 0 && y < 0) ? "terceiro quadrante" :
+                       (x > 0 && y < 0) ? "quarto quadrante" :
+                       (x == 0 || y == 0) ? "sobre um dos eixos" : "origem";
+
+    cout << "O ponto (" << x << ", " << y << ") esta no " << quadrante << endl;
+
+    return 0;
+}
