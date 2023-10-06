56818034cacce1698b455d7e35434372537e7db1
Matheus Beckerath <matheus.beckerath@hotmail.com>
2023-10-06 00:42:51 -0300

    Adicionando 2abcd

---
+++ exercicio2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void){
+    
+    // Letra a 
+    int a, b, c;
+
+    // Letra b 
+    cout << "Digite o valor de a: ";
+    cin >> a;
+    cout << "Digite o valor de b: ";
+    cin >> b;
+
+    // Letra c 
+    c = 4 * a + b / 3 - 5;
+
+    // Letra d 
+    cout << "Resultado da primeira expressao: " << c << endl;
+    
+    return 0;
+}
