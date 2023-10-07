e20648a78bcc4050c8665663ea05aa5f5be9fdf7
Lasbernardes <lasbernardes@uesc.br>
2023-10-07 07:24:52 -0300

    v1 006exerc2f.cpp

--- 006exer2a.cpp
+++ 006exer2a.cpp
@@ -10,6 +10,11 @@ int main(){
     cout<<"c: "<<c<<endl;
     c=4*(a+b)/(3-5);
     cout<<"c: "<<c<<endl;
+    //A utilização dos parenteses, dando prioridade às adição e 
+    //subtração modificam o resultado.
+    c=4*(a+b/3-5);
+    cout<<"c: "<<c<<endl;
+    
     
 
     return 0;
